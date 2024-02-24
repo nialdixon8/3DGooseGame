49492774a1abfca603d58d13dd8ed13526988e13
burgerking59 <oliverbk4@gmail.com>
2024-02-24 23:22:42 +0000

    added player attack

--- enemy.gd
+++ enemy.gd
@@ -9,48 +9,75 @@ var rotation_direction = 0
 @onready var raycast = $RayCast3D
 @onready var raycast2 = $RayCast3D2
 @onready var anim = $enemyGoose/AnimationPlayer
+@onready var timer = $Timer
 
 @export var platform:Node3D
-
+var attack = true
+var pushed = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var direction
 
 func _physics_process(delta):
-	velocity.x = 0
-	velocity.z = 0
-	
-	if raycast.is_colliding():
+	if raycast.is_colliding() and attack:
+		timer.start()
+		attack = false
 		if anim.is_playing():
 			anim.stop()
 		anim.play("neckAttackAction")
 		direction = Vector2(player.global_position.x - global_position.x,player.global_position.z - global_position.z)
-		player.push()
+		player.push(direction)
 		
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
 	if player.platform == platform:
-		velocity.x = clamp(player.global_position.x - global_position.x, -SPEED, SPEED)
-		velocity.z = clamp(player.global_position.z - global_position.z, -SPEED, SPEED)
-		if velocity.x != 0 or velocity.z != 0:
-			if anim.is_playing() and anim.current_animation != "neckAttackAction":
-				anim.play("runAction")
-			if not anim.is_playing():
-				anim.play("runAction")
-		if velocity.x == 0 and velocity.z == 0:
-			if not anim.is_playing():
-				anim.play("idleAction")
-		if Vector2(velocity.z, velocity.x).length() > 0:
-			rotation_direction = Vector2(velocity.z, velocity.x).angle()
-			
-		rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
+		if not pushed:
+			velocity.x = 0
+			velocity.z = 0
+			velocity.x = clamp(player.global_position.x - global_position.x, -SPEED, SPEED)
+			velocity.z = clamp(player.global_position.z - global_position.z, -SPEED, SPEED)
+			if velocity.x != 0 or velocity.z != 0:
+				if anim.is_playing() and anim.current_animation != "neckAttackAction":
+					anim.play("runAction")
+				if not anim.is_playing():
+					anim.play("runAction")
+			if velocity.x == 0 and velocity.z == 0:
+				if not anim.is_playing():
+					anim.play("idleAction")
+			if Vector2(velocity.z, velocity.x).length() > 0:
+				rotation_direction = Vector2(velocity.z, velocity.x).angle()
+			rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
+		else:
+			if velocity.x > 0:
+				velocity.x -= 0.05
+			elif velocity.x < 0:
+				velocity.x += 0.05
+			if velocity.z > 0:
+				velocity.z -= 0.05
+			elif velocity.z < 0:
+				velocity.z += 0.05
+			if abs(velocity.x) <= 1.6 and abs(velocity.z) <= 1.6:
+				pushed = false
 	else:
 		if not anim.is_playing():
 			anim.play("idleAction")
 		velocity.x = 0
 		velocity.z = 0
+		
 	
 	move_and_slide()
+
+func push(direction):
+	velocity.x = 0
+	velocity.z = 0
+	pushed = true
+	velocity.x += direction.x * 4
+	velocity.z += direction.z * 4
+	velocity.y = 3
+
+func _on_timer_timeout():
+	attack = true
--- player.gd
+++ player.gd
@@ -4,6 +4,10 @@ const SPEED = 5
 const JUMP_VELOCITY = 4.5
 var mouse_sensitivity = 0.002
 @onready var downRay = $RayCast3D
+@onready var attackRay = $attackRay
+@onready var attackRay2 = $attackRay2
+@onready var attackRay3 = $attackRay3
+@onready var timer = $Timer
 var platform = null
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -11,18 +15,22 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var anim = $goose2/AnimationPlayer
 
+var attackReady = true
 var rotation_direction = 0
 var velocityY = 0
+var pushed = false
 
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("attack") and attackReady:
+		attack()
+	
 	if downRay.is_colliding():
 		platform = downRay.get_collider().get_parent()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	velocity.x = 0
-	velocity.z = 0
+	
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		if anim.is_playing():
@@ -31,35 +39,88 @@ func _physics_process(delta):
 		anim.play("jumpAction")
 	if position.y < -5:
 		die()
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if Input.is_action_pressed("ui_left"):
-		if velocity.y == 0:
-			anim.play("runAction")
-		velocity.x -= SPEED
-	if Input.is_action_pressed("ui_right"):
-		if velocity.y == 0:
-			anim.play("runAction")
-		velocity.x += SPEED
-	if Input.is_action_pressed("ui_up"):
-		if velocity.y == 0:
-			anim.play("runAction")
-		velocity.z -= SPEED
-	if Input.is_action_pressed("ui_down"):
-		if velocity.y == 0:
-			anim.play("runAction")
-		velocity.z += SPEED
-	if velocity == Vector3.ZERO:
-		anim.play("idleAction")
-	if Vector2(velocity.z, velocity.x).length() > 0:
-		rotation_direction = Vector2(velocity.z, velocity.x).angle()
-		
+	if not pushed:
+		velocity.x = 0
+		velocity.z = 0
+		var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if Input.is_action_pressed("ui_left"):
+			if velocity.y == 0:
+				if anim.is_playing() and anim.current_animation != "neckAttackAction":
+					anim.play("runAction")
+				if not anim.is_playing():
+					anim.play("runAction")
+			velocity.x -= SPEED
+		if Input.is_action_pressed("ui_right"):
+			if velocity.y == 0:
+				if anim.is_playing() and anim.current_animation != "neckAttackAction":
+					anim.play("runAction")
+				if not anim.is_playing():
+					anim.play("runAction")
+			velocity.x += SPEED
+		if Input.is_action_pressed("ui_up"):
+			if velocity.y == 0:
+				if anim.is_playing() and anim.current_animation != "neckAttackAction":
+					anim.play("runAction")
+				if not anim.is_playing():
+					anim.play("runAction")
+			velocity.z -= SPEED
+		if Input.is_action_pressed("ui_down"):
+			if velocity.y == 0:
+				if anim.is_playing() and anim.current_animation != "neckAttackAction":
+					anim.play("runAction")
+				if not anim.is_playing():
+					anim.play("runAction")
+			velocity.z += SPEED
+		if velocity == Vector3.ZERO:
+			if anim.is_playing() and anim.current_animation != "neckAttackAction":
+				anim.play("idleAction")
+			if not anim.is_playing():
+				anim.play("idleAction")
+		if Vector2(velocity.z, velocity.x).length() > 0:
+			rotation_direction = Vector2(velocity.z, velocity.x).angle()
+	else:
+		if velocity.x > 0:
+			velocity.x -= 0.05
+		elif velocity.x < 0:
+			velocity.x += 0.05
+		if velocity.z > 0:
+			velocity.z -= 0.05
+		elif velocity.z < 0:
+			velocity.z += 0.05
+		if abs(velocity.x) <= 1.6 and abs(velocity.z) <= 1.6:
+			pushed = false
 	rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
 
 	move_and_slide()
 
+func attack():
+	attackReady = false
+	timer.start()
+	if anim.is_playing() and not anim.current_animation == "neckAttackAction":
+		anim.stop()
+	anim.play("neckAttackAction")
+	if attackRay.is_colliding():
+		if attackRay.get_collider().name.contains("enemy"):
+			attackRay.get_collider().push(attackRay.get_collider().global_position - global_position)
+	elif attackRay2.is_colliding():
+		if attackRay2.get_collider().name.contains("enemy"):
+			attackRay2.get_collider().push(attackRay2.get_collider().global_position - global_position)
+	elif attackRay3.is_colliding():
+		if attackRay3.get_collider().name.contains("enemy"):
+			attackRay3.get_collider().push(attackRay3.get_collider().global_position - global_position)
+
 func die():
+	pushed = false
 	position = Vector3(0,1,0)
 
-func push():
-	pass
+func push(direction):
+	velocity.x = 0
+	velocity.z = 0
+	pushed = true
+	velocity.x += direction.x * 4
+	velocity.z += direction.y * 4
+	velocity.y = 3
+
+func _on_timer_timeout():
+	attackReady = true
