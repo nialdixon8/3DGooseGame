2067f11d180e8319c0084c391412ba90cfeebc59
burgerking59 <oliverbk4@gmail.com>
2024-02-24 16:45:27 +0000

    added jump animation and enemy

---
+++ CharacterBody3D.gd
@@ -0,0 +1,30 @@
+extends CharacterBody3D
+
+
+const SPEED = 3.0
+const JUMP_VELOCITY = 4.5
+var rotation_direction = 0
+var agroDistance = 10
+@onready var player = get_parent().get_parent().get_node("player").get_node("CharacterBody3D")
+
+@onready var raycast = get_parent().get_node("RayCast3D")
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	if not (player.global_position.z - global_position.z > agroDistance or player.global_position.z - global_position.z < -agroDistance or player.global_position.x - global_position.x > agroDistance or player.global_position.x - global_position.x < -agroDistance):
+
+		velocity.x = clamp(player.global_position.x - global_position.x, -SPEED, SPEED)
+		velocity.z = clamp(player.global_position.z - global_position.z, -SPEED, SPEED)
+		
+		if Vector2(velocity.z, velocity.x).length() > 0:
+			rotation_direction = Vector2(velocity.z, velocity.x).angle()
+			
+		rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
+	
+	move_and_slide()
--- player.gd
+++ player.gd
@@ -7,6 +7,8 @@ var mouse_sensitivity = 0.002
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@onready var anim = $goose1/AnimationPlayer
+
 var rotation_direction = 0
 var velocityY = 0
 
@@ -18,7 +20,10 @@ func _physics_process(delta):
 	velocity.z = 0
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if anim.is_playing():
+			anim.stop()
 		velocity.y = JUMP_VELOCITY
+		anim.play("jumpAction")
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	# Get the input direction and handle the movement/deceleration.
