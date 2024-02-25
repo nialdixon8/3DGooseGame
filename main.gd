5e0021b021431728fd8e8c55a480171156996026
burgerking59 <oliverbk4@gmail.com>
2024-02-25 02:22:21 +0000

    added first level layout

--- main.gd
+++ main.gd
@@ -1,14 +1,10 @@
 extends Node3D
 
-@onready var enemy = $enemy
-@onready var enemy2 = $enemy2
-@onready var enemy3 = $enemy3
-@onready var fallingPlatform = $fallingPlatform
-@onready var fallingPlatform2 = $fallingPlatform2
+@onready var enemy = $SubViewportContainer/SubViewport/enemy
+@onready var enemy3 = $SubViewportContainer/SubViewport/enemy3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,15 +13,6 @@ func _process(delta):
 
 func reset():
 	enemy.position = enemy.defaultPos
-	enemy2.position = enemy2.defaultPos
 	enemy3.position = enemy3.defaultPos
-	fallingPlatform.falling = false
-	fallingPlatform2.falling = false
-	fallingPlatform.gravity = 0
-	fallingPlatform2.gravity = 0
-	fallingPlatform.timer.stop()
-	fallingPlatform2.timer.stop()
-	fallingPlatform.position = fallingPlatform.defaultPos
-	fallingPlatform2.position = fallingPlatform2.defaultPos
 	
 	
